4bdbfe6edc7d02b2fa49d2890b7f5f40550b63aa
redBOY <43438363+redBOY@users.noreply.github.com>
2022-09-12 01:54:11 +0530

    UC2

---
+++ UserReg.java
@@ -0,0 +1,22 @@
+package bridgelabzRegex;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserReg {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the last name : ");
+            String lastName = sc.nextLine();
+
+            boolean fName = Pattern.matches("^[A-Z]+[a-zA-Z]{2,}[0-9]*$", lastName);
+
+            if (fName) {
+                System.out.println("lastname is correct ");
+            } else {
+                System.out.println("lastname is incorrect ");
+            }
+
+        }
+
+    }
+
