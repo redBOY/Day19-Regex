c9891ed6d3196d6d6391acd7efa8178c769f5b43
redBOY <43438363+redBOY@users.noreply.github.com>
2022-09-12 02:01:48 +0530

    UC3

---
+++ UserReg.java
@@ -0,0 +1,21 @@
+package bridgelabzRegex;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class UserReg {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first name: ");
+        String firstName = sc.next();
+        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+        Matcher match = pattern.matcher(firstName);
+        if (match.find() && match.group().equals(firstName)) {
+            System.out.println("valid FirstName");
+        } else {
+            System.out.println("invalid FirstName");
+        }
+
+    }
+
+}
